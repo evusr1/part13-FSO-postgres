cf18482fba2cbddd9394d98ab3bed03c40c15873
evusr1 <127464885+evusr1@users.noreply.github.com>
2023-07-03 03:54:03 -0700

    13.19; add user readings list

--- models/index.js
+++ models/index.js
@@ -1,10 +1,15 @@
 const Blog = require('./blog')
 const User = require('./user')
+const UserReading = require('./user_reading')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
+User.belongsToMany(Blog, { through: UserReading, as: 'marked_blogs' })
+Blog.belongsToMany(Blog, { through: UserReading, as: 'users_marked' })
+
 module.exports = {
   Blog,
-  User
+  User,
+  UserReading
 }
---
+++ models/user_reading.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize')
+const { sequelize } = require('../utils/db')
+
+class UserReading extends Model {}
+
+UserReading.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: { model: 'users', key: 'id' }
+  },
+  blogId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: { model: 'blogs', key: 'id' }
+  },
+  isRead: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  },
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'user_reading'
+})
+
+module.exports = UserReading
